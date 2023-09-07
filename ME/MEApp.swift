e1ef6b7448936ba77ee4b24e9b38b695eee32858
laurensaggar <saggar.4@osu.edu>
2023-09-06 20:20:07 -0600

    Initial Commit

---
+++ ME/MEApp.swift
@@ -0,0 +1,17 @@
+//
+//  MEApp.swift
+//  ME
+//
+//  Created by Lauren Saggar on 9/6/23.
+//
+
+import SwiftUI
+
+@main
+struct MEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
