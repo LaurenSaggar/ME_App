66ef09789fb85482a2cacce191db32fd6899fc96
laurensaggar <saggar.4@osu.edu>
2023-09-23 17:44:52 -0400

    Added more model data

--- ME/Models/ModelData.swift
+++ ME/Models/ModelData.swift
@@ -10,14 +10,32 @@ import Combine
 
 final class ModelData: ObservableObject {
     @Published var categories: [Category] = [
-        Category(id: 1, name: "Period Essentials", completion: 0.80, color: "systemPink", items: [CategoryItem]()),
-        Category(id: 2, name: "Increase Energy", completion: 0.56, color: "orange", items: [CategoryItem]()),
+        Category(id: 1, name: "Period Essentials", completion: 0.80, color: "systemPink", items: [
+            CategoryItem(id: 1, categoryName: "Period Essentials", itemName: "Get pads", isChecked: true),
+            CategoryItem(id: 1, categoryName: "Period Essentials", itemName: "Get tampons", isChecked: true),
+            CategoryItem(id: 1, categoryName: "Manage Pain", itemName: "Pack Advil in to-go bag", isChecked: true)]),
+        
+        Category(id: 2, name: "Increase Energy", completion: 0.56, color: "orange", items: [
+            CategoryItem(id: 2, categoryName: "Increase Energy", itemName: "Go for a walk", isChecked: true),
+            CategoryItem(id: 2, categoryName: "Increase Energy", itemName: "Avoid trigger foods", isChecked: true)]),
+        
         Category(id: 3, name: "Relax Body", completion: 0.37, color: "systemMint", items: [
-            CategoryItem(id: 1, categoryName: "Relax Body", itemName: "Rest", isChecked: true),
-            CategoryItem(id: 2, categoryName: "Relax Body", itemName: "Take a hot bath", isChecked: false),
-            CategoryItem(id: 3, categoryName: "Relax Body", itemName: "Drink warm tea", isChecked: false)]),
-        Category(id: 4, name: "Manage Pain", completion: 0.70, color: "systemCyan", items: [CategoryItem]()),
-        Category(id: 5, name: "Improve Mood", completion: 0.28, color: "blue", items: [CategoryItem]()),
-        Category(id: 6, name: "Feel Supported", completion: 0.52, color: "purple", items: [CategoryItem]())
+            CategoryItem(id: 3, categoryName: "Relax Body", itemName: "Rest", isChecked: true),
+            CategoryItem(id: 3, categoryName: "Relax Body", itemName: "Take a hot bath", isChecked: true),
+            CategoryItem(id: 3, categoryName: "Relax Body", itemName: "Drink warm tea", isChecked: true)]),
+        
+        Category(id: 4, name: "Manage Pain", completion: 0.70, color: "systemCyan", items: [
+            CategoryItem(id: 4, categoryName: "Manage Pain", itemName: "Apply a heating pad", isChecked: true),
+            CategoryItem(id: 4, categoryName: "Manage Pain", itemName: "Take advil", isChecked: true)]),
+        
+        Category(id: 5, name: "Improve Mood", completion: 0.28, color: "blue", items: [
+            CategoryItem(id: 5, categoryName: "Improve Mood", itemName: "Eat ice cream", isChecked: true),
+            CategoryItem(id: 5, categoryName: "Improve Mood", itemName: "Watch a movie", isChecked: true),
+            CategoryItem(id: 5, categoryName: "Improve Mood", itemName: "Have a night in", isChecked: true)]),
+        
+        Category(id: 6, name: "Feel Supported", completion: 0.52, color: "purple", items: [
+            CategoryItem(id: 6, categoryName: "Feel Support", itemName: "Talk to a friend", isChecked: true),
+            CategoryItem(id: 6, categoryName: "Feel Support", itemName: "Talk to a partner", isChecked: true),
+            CategoryItem(id: 6, categoryName: "Feel Support", itemName: "Mention period to a colleague", isChecked: true),])
     ]
 }
